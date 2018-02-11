e93287a3c1714a77a7886f07cac013be1d9d76e6
Sebastian Amara <sqamara@ucsd.edu>
2018-02-10 18:32:07 -0800

    plotting of data

--- plot.py
+++ plot.py
@@ -1,10 +1,27 @@
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
+
+def reject_outliers(data):
+    print(np.median(data))
+    
+    res = []
+    for i in range(len(data)):
+        if abs(data[i] - np.median(data)) < 5: 
+            res.append(data[i])
+        else:
+            res.append(res[-1])
+
+    return res
+
 
 def main():
-	with open('results.pickle', 'rb') as file:
-		results1, results2 = pickle.load(file)
+    with open('results.pickle', 'rb') as file:
+        results1, results2 = pickle.load(file)
 
+    results1 = reject_outliers(results1)
+    results2 = reject_outliers(results2)
+    
     plt.subplot(2, 1, 1)
     plt.plot(range(len(results1)), results1)
     plt.ylabel('distance [cm]')
@@ -19,4 +36,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+    main()
