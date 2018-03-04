e91fe377ddc62b739669e523ce6a5cebf5c17229
Sebastian Amara <sqamara@ucsd.edu>
2018-03-04 03:23:07 +0000

    got sonic sw delay

--- measure_sensor_delay.py
+++ measure_sensor_delay.py
@@ -1,4 +1,18 @@
 import time
+import RPi.GPIO as GPIO
 from sonic_sensor import SonicSensor
 
-sensor1 = SonicSensor(trig_pin=18, echo_pin=24, numbering_mode=GPIO.BCM)
+sensor = SonicSensor(trig_pin=18, echo_pin=24, numbering_mode=GPIO.BCM)
+
+iterations = 10000
+
+for i in range(10):
+    StartTime = time.time()
+    
+    for i in range(iterations):
+        sensor.get_distance()
+                
+    
+    StopTime = time.time()
+
+print((StopTime-StartTime)/iterations)
